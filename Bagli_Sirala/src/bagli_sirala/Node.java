10acfb3d54a400c078130931c474dded8eaee0b3
Abdullah Samet Eroğlu <128939551+erogluusamett@users.noreply.github.com>
2024-04-03 20:57:06 +0300

    Add files via upload

---
+++ Bagli_Sirala/src/bagli_sirala/Node.java
@@ -0,0 +1,11 @@
+package bagli_sirala;
+
+public class Node {
+    int data;
+    int adres;
+
+    public Node(int data, int adres) {
+        this.data = data;
+        this.adres = adres;
+    }
+}
